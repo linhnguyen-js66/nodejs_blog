c11f10d61a4d3efc19112b9fafecb4c2a76f7fb1
Linh. Nguyen Ha - CMC Global DU1.3 <nhlinh3@cmcglobal.vn>
2022-06-30 10:47:37 +0700

    add css scss

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
-const express = require("express");
-const morgan = require("morgan")
+import express from 'express';
+import { engine } from 'express-handlebars';
+import morgan from 'morgan';
+import path from 'path';
+import {__dirname} from './dirname_format.js'
 const app = express();
 const port = 3000;
-const handlebars = require("express-handlebars")
 //Dinh nghia route
 /**
  * Neu thay doi "/" thanh "trang chu" thi khi chay localhost phai them 
  * nhu sau  http://localhost:${port}/trangchu
  */
-
+//xu ly static file
+app.use(express.static(path.join(__dirname,'resources/public')))
 //template engine
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
-
-
+app.engine('hbs', engine({
+  extname:'.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views',path.join(__dirname,'resources/views'))
 //http logger/ log nhung request tu client
 app.use(morgan('combined'))
 app.get("/trang-chu", (req, res) => res.render('home'));
+app.get("/tin-tuc", (req, res) => res.render('news'));
 //listen local host tren browser
 app.listen(port, () => {
   console.log(`Example app listening on port at http://localhost:${port}`);
