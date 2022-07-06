188b2d5c5621c2772df9c8a8c7de5bc3bd5b3530
Linh. Nguyen Ha - CMC Global DU1.3 <nhlinh3@cmcglobal.vn>
2022-07-06 09:57:26 +0700

    test husky3

--- src/app/controllers/NewController.js
+++ src/app/controllers/NewController.js
@@ -1,12 +1,12 @@
 const NewController = {
   // GET / news
-  index: (req, res) => {
-    res.render("news");
-  },
-  // [GET] /tin-tuc:slug
-  show: (req, res) => {
-    res.send("TEST!!!!");
-  },
+          index: (req, res) => {
+            res.render("news");
+          },
+          // [GET] /tin-tuc:slug
+          show: (req, res) => {
+            res.send("TEST!!!!");
+          },
 };
 
 export default NewController;
